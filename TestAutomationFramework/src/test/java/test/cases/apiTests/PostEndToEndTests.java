c5cebefe5eda650d811d638ac2f2152252d345f8
Simeon Velinov <132641869+SimeonVelinov@users.noreply.github.com>
2023-10-09 20:25:51 +0300

    Post Controller Refactor
    Refactoring of post controller and separating End To End tests

---
+++ TestAutomationFramework/src/test/java/test/cases/apiTests/PostEndToEndTests.java
@@ -0,0 +1,18 @@
+package test.cases.apiTests;
+
+import org.junit.jupiter.api.Test;
+
+public class PostEndToEndTests extends PostControllerTests{
+
+    @Test
+    public void CRUD_Post_Flow_With_Valid_Data_Successfully() {
+        create_Post_With_Valid_Data();
+        System.out.println("Post created successfully");
+        edit_Post_With_Valid_Data();
+        System.out.println("Post edited successfully");
+        like_Post_When_Post_Exists();
+        System.out.println("Post liked successfully");
+        delete_Post_When_Post_Exists();
+        System.out.println("Post deleted successfully");
+    }
+}
