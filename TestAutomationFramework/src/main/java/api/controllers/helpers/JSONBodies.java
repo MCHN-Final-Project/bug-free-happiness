c4244efdfefdf224219181ad113695f171f77c2e
petarhlebaroff <petarhlebaroff@gmail.com>
2023-10-09 13:17:51 +0300

    Add helpers package

---
+++ TestAutomationFramework/src/main/java/api/controllers/helpers/JSONBodies.java
@@ -0,0 +1,32 @@
+package api.controllers.helpers;
+
+public class JSONBodies {
+
+    public static final String CREATE_SKILL_BODY = "{\n" +
+            "    \"category\": {\n" +
+            "        \"id\": %s,\n" +
+            "        \"name\": \"%s\"\n" +
+            "    },\n" +
+            "    \"skill\": \"%s\",\n" +
+            "    \"skillId\": 0\n" +
+            "}";
+
+    public static final String CREATE_USER_BODY = "{\n" +
+            "  \"authorities\": [\n" +
+            "    \"ROLE_USER\"\n" +
+            "  ],\n" +
+            "  \"category\": {\n" +
+            "    \"id\": 102,\n" +
+            "    \"name\": \"Actor\"\n" +
+            "  },\n" +
+            "  \"confirmPassword\": \"%s\",\n" +
+            "  \"email\": \"%s\",\n" +
+            "  \"password\": \"%s\",\n" +
+            "  \"username\": \"%s\"\n" +
+            "}";
+
+    public static final String SEND_REQUEST_BODY = "{\n" +
+            "    \"id\": %d,\n" +
+            "    \"username\": \"%s\"\n" +
+            "}";
+}
